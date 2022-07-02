eaa8c0ae69872e3f4a034c5ad8b81909d8a1e9d6
Akhmed Umarov <101022306+akhmed-umarov@users.noreply.github.com>
2022-07-02 10:11:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,130 +1,18 @@
 const path = require('path') ; let fs = require('fs') ; let http = require('http'); 
 const EventEmitter = require('events'); 
 
-
-
-
-// const DirName = path.join(__dirname , 'public'); 
-
-// fs.mkdir(DirName , (er)=>{{ 
-//    if (er){ 
-//       throw er
-//    }
-// }})
-
-
-
-// fs.writeFile(path.join(__dirname , 'public' , 'index.html') , '' , (er)=> {
-// if (er){ 
-// throw er
-// }
-// })
-
-// fs.writeFile(path.join(__dirname , 'public' , 'center.html') , '' ,  (er)=> {
-//    if (er){ 
-//    throw er
-//    }
-//    })
-
-//    fs.writeFile(path.join(__dirname , 'public' , 'error.html') , '' , (er)=> {
-//       if (er){ 
-//       throw er
-//       }
-// //       })
-// const server = http.createServer((req , res)=> { 
-
-//    let filePath = path.join(__dirname , 'publick' , req.url === '/' ? 'index.html' : req.url)
-
-
-
-//    //более сложный путь проверки и добавления '.html" к пути req.url
-//    const ext = path.extname(filePath); 
-//    if (!ext){ 
-//       filePath += '.html'
-//    }
-
-   
-//    //для того чтобы подключить таблицы стилей мне надо проверить какой вид контента у меня передается на сервер и в зависимости от этого действовать
-
-//    let contentType = 'text/html'
-
-
-//    switch (ext) { 
-//       case '.css': 
-//       contentType = 'text/css'
-//       break
-//       case '.js':
-//       contentType = 'text/js' 
-//       break 
-//       default: 
-//       contentType = 'text/html'
-//    }
-
-// //таким образом мы добавили все варианты элементов на сайт
-
-
-
-
-   
-//    console.log(filePath); 
-
-     
-
-
-
-
-
-
-
-
-
-
-
-//оригинал
-
-// const server = http.createServer( (request , responce)=>{ 
-
-   
-//    let filePath = path.join(__dirname , 'public' , request.url === "/" ? 'index.html' : request.url)
-
-
-
-//    const ext = path.extname(filePath); 
-//    if (!ext) { 
-//       filePath += '.html'
-//    }
-   
-//    // console.log(request.url)
-
-//    let typeFiles = 'text/html'
-
-//    switch (ext){ 
-//       case '.css' :
-//    typeFiles = 'text/css' 
-//       case '.js': 
-//    typeFiles = 'text/js'
-//       default: 
-//    typeFiles = 'text/html'
-//    } 
-
-// console.log(filePath)
-
-//end original
 const server = http.createServer((request , responce)=> { 
 
    let filePath = path.join(__dirname , 'public' , request.url === '/' ? 'index.html' : request.url)
-
-
-
+   
    //более сложный путь проверки и добавления '.html" к пути req.url
    const ext = path.extname(filePath); 
    if (!ext){ 
       filePath += '.html'
    }
-
    
    //для того чтобы подключить таблицы стилей мне надо проверить какой вид контента у меня передается на сервер и в зависимости от этого действовать
-
+   
    let typeFiles = 'text/html'
 
 
@@ -138,17 +26,6 @@ const server = http.createServer((request , responce)=> {
       default: 
       typeFiles = 'text/html'
    }
-
-   // let typeFiles = 'text/html'
-
-   //    switch (ext){ 
-   //       case '.css':
-   //    typeFiles = 'text/css' 
-   //       case '.js': 
-   //    typeFiles = 'text/js'
-   //       default: 
-   //    typeFiles = 'text/html'
-   //    } 
 
 //таким образом мы добавили все варианты элементов на сайт
 
@@ -187,11 +64,6 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT , ()=> { 
    console.log(`Server has been started on ${PORT}...`)
 })
-// server.listen(3000 , (er)=> { 
-//    if (er){ 
-//       throw er
-//    }
-// })
 
 
 
